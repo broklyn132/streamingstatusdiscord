4771c080d908ea45f2784dde4ece08c6e650ec20
broklyn132 <96223662+broklyn132@users.noreply.github.com>
2021-12-16 13:09:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = " HamsShop ┊ Store Lengkap & Termurah ┊ 24/7 Online ┊ https://dsc.gg/hamsshop ", url = "link twicth"))
+  await client.change_presence(activity = discord.Streaming(name = " HamsShop ┊ Store Lengkap & Termurah ┊ 24/7 Online ┊ https://dsc.gg/hamsshop ", url = "https://www.twitch.tv/brooklyn3213"))
 
 
 keep_alive.keep_alive()
